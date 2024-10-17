23679e069503fa73cc8f9a5244568e1ee849782c
Annurdien Rasyid <annurdien.dev@gmail.com>
2024-10-17 10:59:41 +0700

    refactor: improve security

--- src/serverless.ts
+++ src/serverless.ts
@@ -3,6 +3,8 @@ import serverlessExpress from '@codegenie/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
+
 
 let server: Handler;
 
@@ -10,6 +12,7 @@ async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.enableVersioning({ type: VersioningType.URI });
+  app.use(helmet());
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
