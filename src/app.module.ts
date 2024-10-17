e8d7b8cbecd14fbd67b00c7facad9bb835c793d6
Annurdien Rasyid <annurdien.dev@gmail.com>
2024-10-17 11:00:01 +0700

    feat: add throtler

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,30 @@ import { OpenrouterModule } from './openrouter/openrouter.module';
 import { SharedModule } from './shared/shared.module';
 import { PatternModule } from '~pattern/pattern.module';
 import { SambaNovaModule } from './samba-nova/samba-nova.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [GeminiModule, OpenrouterModule, SharedModule, PatternModule, SambaNovaModule],
+  imports: [
+    GeminiModule,
+    OpenrouterModule,
+    SharedModule,
+    PatternModule,
+    SambaNovaModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // 10 request per minute per IP
+        limit: 10
+      }
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
